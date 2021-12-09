f4d856cf78d4163322a6bb461242420f9b33eb7c
Mohamed Ashiq <mohamedashiq301@gmail.com>
2021-12-09 22:03:51 +0530

    Add some files

--- MyApp/app/src/main/java/com/example/myapp/LoginActivity.java
+++ MyApp/app/src/main/java/com/example/myapp/LoginActivity.java
@@ -10,12 +10,14 @@ import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
 
     EditText EditTextFullName,EditTextUserName;
     Button ButtonLogin;
     SharedPreferences sharedPreferences;
+    SharedPreferences.Editor editor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,16 +29,38 @@ public class LoginActivity extends AppCompatActivity {
         ButtonLogin=findViewById(R.id.ButtonLogin);
         setSharedPreferences();
 
+
         View.OnClickListener clickAction= new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String fullname=EditTextFullName.getText().toString();
-                Intent intent=new Intent(LoginActivity.this,MyNotesActivity.class);
-                intent.putExtra(AppConstant.Full_Name,fullname);
-              startActivity(intent);
+                String username=EditTextUserName.getText().toString();
+                if(!TextUtils.isEmpty(fullname) && !TextUtils.isEmpty(username)) {
+                    Intent intent = new Intent(LoginActivity.this, MyNotesActivity.class);
+                    intent.putExtra(AppConstant.Full_Name, fullname);
+                    startActivity(intent);
+                    //Login
+                    saveLoginstatus();
+                    savefullname(fullname);
+                }else {
+                    Toast.makeText(LoginActivity.this, "FullName and UserName Can't be empty", Toast.LENGTH_SHORT).show();
+                }
             }
         };
         ButtonLogin.setOnClickListener(clickAction);
+    }
+
+    private void savefullname(String fullname) {
+        editor=sharedPreferences.edit();
+        editor.putString(PrefConstant.full_name,fullname);
+        editor.apply();
+
+    }
+
+    private void saveLoginstatus() {
+        editor=sharedPreferences.edit();
+        editor.putBoolean(PrefConstant.Is_Logged_In,true);
+        editor.apply();
     }
 
     private void setSharedPreferences() {
--- MyApp/app/src/main/java/com/example/myapp/PrefConstant.java
+++ MyApp/app/src/main/java/com/example/myapp/PrefConstant.java
@@ -2,4 +2,10 @@ package com.example.myapp;
 
 public class PrefConstant {
     public static String Shared_Preference_Notes="NotesAppPerf";
+    public static String Is_Logged_In="is_logged_in";
+    public static String full_name;
+
+    static {
+        full_name = "fullname";
+    }
 }
