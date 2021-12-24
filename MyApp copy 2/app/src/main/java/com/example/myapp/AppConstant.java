674959c745d09c984be0678f91b923abd6124791
Mohamedashiq301 <mohamedashiq301@gmail.com>
2021-12-24 12:05:34 +0530

    Add some files

--- MyApp copy 2/app/src/main/java/com/example/myapp/AppConstant.java
+++
@@ -1,7 +0,0 @@
-package com.example.myapp;
-
-public class AppConstant {
-    public static String Full_Name="fullname";
-    public static String TITLE="title";
-    public static String DESCRIPTION="description";
-}
--- MyApp/app/src/main/java/com/example/myapp/clicklinstener/ItemClickListener.kt
+++
@@ -1,7 +0,0 @@
-package com.example.myapp.clicklinstener
-
-import com.example.myapp.model.Notes
-
-interface ItemClickListener {
-    fun onClick(notes:Notes)
-}
