0b392978f21fb1e61ca55af9b607ee001264c6d8
Mohamedashiq301 <mohamedashiq301@gmail.com>
2022-01-06 22:34:57 +0530

    Add some files

--- MyApp copy/app/src/main/java/com/example/myapp/NotesApp.kt
+++ MyApp copy/app/src/main/java/com/example/myapp/NotesApp.kt
@@ -1,11 +1,13 @@
 package com.example.myapp
 
 import android.app.Application
+import com.androidnetworking.AndroidNetworking
 import com.example.myapp.db.NotesDatabase
 
 class NotesApp : Application() {
     override fun onCreate() {
         super.onCreate()
+        AndroidNetworking.initialize(applicationContext)
     }
 
     fun getNotesDb(): NotesDatabase {
