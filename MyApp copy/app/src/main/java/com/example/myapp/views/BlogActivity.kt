32e8d11a07d08380c4359950b3651231f9c7deb4
Mohamedashiq301 <mohamedashiq301@gmail.com>
2022-01-07 23:18:18 +0530

    Add some files

--- MyApp copy/app/src/main/java/com/example/myapp/views/BlogActivity.kt
+++ MyApp copy/app/src/main/java/com/example/myapp/views/BlogActivity.kt
@@ -2,8 +2,19 @@ package com.example.myapp.views
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.example.myapp.Adapter.BlogAdapter
 import com.example.myapp.R
+import com.androidnetworking.error.ANError
+
+import org.json.JSONArray
+
+import com.androidnetworking.interfaces.JSONArrayRequestListener
+
+import com.androidnetworking.AndroidNetworking
+import com.androidnetworking.common.Priority
+
 
 class BlogActivity : AppCompatActivity() {
 
@@ -13,6 +24,22 @@ class BlogActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_blog)
         bindViews()
+        getBlogs()
+    }
+
+    private fun getBlogs() {
+        AndroidNetworking.get("https://fierce-cove-29863.herokuapp.com/getAllUsers/{pageNumber}")
+                .setPriority(Priority.LOW)
+                .build()
+                .getAsJSONArray(object : JSONArrayRequestListener {
+                    override fun onResponse(response: JSONArray) {
+                        // do anything with response
+                    }
+
+                    override fun onError(error: ANError) {
+                        // handle error
+                    }
+                })
     }
 
     private fun bindViews() {
@@ -20,6 +47,10 @@ class BlogActivity : AppCompatActivity() {
     }
 
     private fun setupRecyclerView(){
-
+        val BlogAdapter=BlogAdapter()
+        val linearLayoutManager=LinearLayoutManager(this)
+        linearLayoutManager.orientation=RecyclerView.VERTICAL
+        recyclerviewBlogs.layoutManager=linearLayoutManager
+        recyclerviewBlogs.adapter=BlogAdapter
     }
 }
